89da2f36f8b8474e15a1c5786a2665338d097044
diman <realun00@gmail.com>
2023-11-19 18:00:55 +0200

    feat: commitlint and lint-staged added

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React test</h1>
       <div className='card'>
         <button type='button' onClick={() => setCountState((count) => count + 1)}>
           count is {countState}
